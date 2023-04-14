b64193377cbf0acdd649e5da2f0623491de528b0
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-04-14 16:50:55 +0300

    1404

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 4444;
 99999999;
+
+const save = (key, value) => {
+    try {
+      const serializedState = JSON.stringify(value);
+      localStorage.setItem(key, serializedState);
+    } catch (error) {
+      console.error("Set state error: ", error.message);
+    }
+  };
+  
+  const load = key => {
+    try {
+      const serializedState = localStorage.getItem(key);
+      return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+      console.error("Get state error: ", error.message);
+    }
+  };
+  
+  export default {
+    save,
+    load,
+  };
---
+++ node-tut/index.js
@@ -0,0 +1,2 @@
+const message = 'tratata';
+console.log('message');
